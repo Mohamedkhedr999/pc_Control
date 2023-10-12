1a2bb286d01474e4f667a9f5e66c6e902a04b893
Mohamed khedr <80976641+Mohamedkhedr999@users.noreply.github.com>
2023-10-12 19:48:30 +0300

    Update Server_Handler.cpp

--- Server_Handler.cpp
+++ Server_Handler.cpp
@@ -28,8 +28,8 @@ bool server_handler::server_intit()
         exit(EXIT_FAILURE); 
     } 
     address.sin_family = AF_INET;
-    address.sin_addr.s_addr =inet_addr("192.168.1.108");//inet_addr("192.168.1.108");//INADDR_ANY; ;;    //inet_addr("192.168.1.255");
-    address.sin_port = htons(8888);
+    address.sin_addr.s_addr =inet_addr("0.0.0.0"); // pc ip should be here
+    address.sin_port = htons(8888); 
     if (bind(socket_fd, (struct sockaddr*)&address, sizeof(address) ) < 0)
     {
         return false;
